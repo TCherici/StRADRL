528bf4fbd61f91a79dfc24fc6d6c9caa66a4a5a5
TheTazza <tcherici@gmail.com>
2018-02-15 10:55:00 +0100

    oepsie

--- model/fc_model.py
+++ model/fc_model.py
@@ -15,8 +15,8 @@ SEED = 4444#3000
 # https://github.com/muupan/async-rl/blob/master/a3c_ale.py
 def fc_initializer(input_channels, dtype=tf.float32):
     def _initializer(shape, dtype=dtype, partition_info=None):
-        #d = 1.0 / np.sqrt(input_channels)
-        d = np.sqrt(1/input_channels)
+        d = 1.0 / np.sqrt(input_channels)
+        #d = np.sqrt(1/input_channels)
         return tf.random_uniform(shape, minval=-d, maxval=d)#, seed=SEED)
     return _initializer
 
